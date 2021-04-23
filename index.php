4919b5e870ee6c16acd1662b0b40961f4d311b36
Camilo Espinosa <ccabreraespinosa@hotmail.com>
2021-04-23 10:53:00 -0500

    Iniciando el curso del sena

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi primer codigo en PHP</title>
+</head>
+<body>
+    <h1>Hola Mundo</h1>
+    <h5>Bienvenidos</h5>
+
+<?php
+
+    $nombre = "Camilo Espinosa";
+    $apellido = "Camila Azuero";
+    echo "Hola $nombre";
+    echo "<br>";
+    echo "Hola ".$apellido;
+    echo "<br>";
+    $nombre = 100;
+    echo $nombre;
+
+?>
+
+</body>
+</html>
